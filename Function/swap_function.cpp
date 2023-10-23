b2557e3a4f80c59f5dede12336fc74a321db813b
Khanh Nham <karidevilmh@gmail.com>
2023-10-23 15:19:50 -0700

    setup

--- Function/swap_function.cpp
+++ Function/swap_function.cpp
@@ -1,5 +1,20 @@
 /*
-Make a function called 'swap' that designed to swap two number 
-input: int a, int b
-output: a b (but swapped)
+Task: Create a Swap Function
+
+What to Do:
+Make a function in C++ that switches the values of two numbers.
+
+Details:
+
+Function Name: swap
+Parameters: Two numbers (int &a, int &b)
+Result: After using the function, a and b should have each other's values.
+Where to Put It:
+
+Folder: Function
+File: swap_function.cpp
+Tips:
+
+Use references to make the swap.
+Test to make sure it works!
 */
